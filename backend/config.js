8312e071e3de9c06f0811819f49c614f13b71197
vishwa prasanth <109272835+vishwaprasanth@users.noreply.github.com>
2023-10-10 19:00:49 +0530

    first

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 export  const PORT = 5555;
 
-export const mongoDBURL = 'mongodb+srv://Vishwa:<password>@book-store-mern.bxgvuqt.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp'
+export const mongoDBURL = 'mongodb+srv://Vishwa:Vibasrigotha@book-store-mern.bxgvuqt.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp'
 
 // please create a free database yourself.
 // this databse will be delete after the tutorial
